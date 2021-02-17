5a254bd9038e1dc4d2785603707625ccfd19b0df
Iaroslav Silkin <maxfri1995@gmail.com>
2021-02-17 15:03:46 +0300

    feat: add algorithm and solution of this task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+    if (arguments.length === 0 || matrix.length === 0) {
+        return [];
+    }
+
+    return matrix.map(function (element, index) {
+        if (index % 2 == 0 || index == 0) {
+            return element;
+        } else {
+            return element.reverse();
+        }
+    }).flat();
 }
